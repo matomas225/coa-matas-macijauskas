6df46589d4575867af83607b478662367ed5423c
matomas225 <matomas225@gmail.com>
2025-03-26 22:20:50 +0200

    added scss, added global routes(problem with TypeScript TODO), added more font weights

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import NavItem from "./NavItem";
+//@ts-ignore Note: for now ignore needs more setup but it works!
+import { routes } from "@routes";
+
+const Wrapper = styled.header`
+  background-color: black;
+  padding: 20px 10px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: end;
+`;
+
+const UnsortedList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  list-style-type: none;
+`;
+
+const Navigation: React.FC = () => {
+  console.log(routes.home);
+  return (
+    <Wrapper>
+      <Nav>
+        <UnsortedList>
+          <NavItem route={routes.home} name="Home" />
+          <NavItem route="/" name="About" />
+          <NavItem route="/" name="Contact" />
+        </UnsortedList>
+      </Nav>
+    </Wrapper>
+  );
+};
+
+export default Navigation;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import GlobalStyle from "./styles/GlobalStyle.ts";
+import "./styles/style.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalStyle />
     <App />
   </StrictMode>
 );
