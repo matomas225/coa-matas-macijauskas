8f21f3bd358661b5f40ad09b30b2e112909895c9
matomas225 <matomas225@gmail.com>
2025-03-30 17:43:33 +0300

    implement api usage (Test)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
+import Navigation from "@/components/Navigation/Navigation.tsx";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,34 @@
-import React from "react";
-import Navigation from "../../components/Navigation/Navigation";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./Home.scss";
 
 const Home: React.FC = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchTestData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/data");
+      setItems(response.data.data);
+    } catch (err) {
+      setError("Error while fetching data");
+    }
+  };
+
+  useEffect(() => {
+    fetchTestData();
+  }, []);
+
   return (
     <>
-      <Navigation />
       <h1>Home Page</h1>
+      {error && <p>{error}</p>}
+      <div className="items">
+        {items && <p>Data From Backend:</p>}
+        {items.map((item, i) => {
+          return <p key={i + 1}>{item}</p>;
+        })}
+      </div>
     </>
   );
 };
