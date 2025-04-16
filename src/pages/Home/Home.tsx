f46fbdf24a4f2d0cd2addbfdcdfdf4689b8d8546
matomas225 <matomas225@gmail.com>
2025-04-16 22:54:37 +0300

    Add song playback

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
+import axios from "axios";
+import { apiPaths } from "@/services/api";
 
 const Home: React.FC = () => {
+  const [songs, setSongs] = useState([]);
+  console.log(songs);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      const response = await axios.get(apiPaths.getSongs);
+
+      setSongs(response.data);
+    };
+    getSongs();
+  }, []);
+
   return (
     <>
       <h1>Home Page</h1>
+      {songs.length === 0 ? (
+        <p>Loading..</p>
+      ) : (
+        <div>
+          {songs.map((songUrl, index) => (
+            <div key={index}>
+              <audio controls>
+                <source src={songUrl} type="audio/mpeg" />
+              </audio>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
