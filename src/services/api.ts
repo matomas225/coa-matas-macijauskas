4dc90c074c7003d10b3aaa74c213a8a9bb2dc760
matomas225 <matomas225@gmail.com>
2025-04-16 22:54:37 +0300

    Add song playback

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 const API_URL = import.meta.env.VITE_API_URL;
 const users = `${API_URL}/users`;
+const songs = `${API_URL}/songs`;
 
 export const apiPaths = {
   getUsers: `${users}/getUsers`,
   postUser: `${users}/postUser`,
   loginUser: `${users}/loginUser`,
   isLogedIn: `${users}/isLogedIn`,
+  getSongs: `${songs}`,
 };
