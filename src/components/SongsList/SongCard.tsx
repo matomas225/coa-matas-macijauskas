0a986f98cc6504377e52749b34351fbbf88429ed
matomas225 <matomas225@gmail.com>
2025-05-05 21:03:31 +0300

    implement song and image from backend todo typescript

--- src/components/SongsList/SongCard.tsx
+++ src/components/SongsList/SongCard.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useRef, useState } from "react";
 
-export const SongCard: React.FC = () => {
+export const SongCard: React.FC = ({ song, image, title, artist, songId }) => {
+  const songRef = useRef(null);
+
   return (
     <div className="card-wrapper">
-      <div>Image</div>
-      <h3>Song Title</h3>
-      <p>Song Info...</p>
+      <audio ref={songRef} src={song} id={songId} />
+      <div className="image-wrapper">
+        <img src={image} />
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          className="play-icon"
+          onClick={() => songRef.current.play()}
+        />
+      </div>
+      <h3>{title}</h3>
+      <p>{artist}</p>
     </div>
   );
 };
--- src/components/SongsList/SongsList.tsx
+++ src/components/SongsList/SongsList.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import "./SongList.scss";
 import { SongCard } from "./SongCard";
+import axios from "axios";
+import { apiPaths } from "@/services/api";
 
 export const SongsList: React.FC = () => {
+  const [songs, setSongs] = useState(null);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      const results = await axios.get(apiPaths.getSongs);
+      if (results.data) {
+        setSongs(results.data);
+      }
+    };
+
+    getSongs();
+  }, []);
+
   return (
     <section className="songs-wrapper">
       <h1>Popular Songs</h1>
       <div className="songs">
-        <SongCard />
-        <SongCard />
-        <SongCard />
+        {songs
+          ? songs.map((song) => {
+              return (
+                <SongCard
+                  key={song.id}
+                  song={song.songPath}
+                  image={song.imagePath}
+                  title={song.title}
+                  artist={song.artist}
+                  songId={song.id}
+                />
+              );
+            })
+          : null}
       </div>
     </section>
   );
